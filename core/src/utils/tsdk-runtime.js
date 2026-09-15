3955442a32d58520ba6fff3a5d4be0a0978f26f9
xxxscarlxrd404 <136799429@qq.com>
2026-09-15 09:52:38 +0800

    feature: 更新wasm版本、优化比熊激活和宠物日志

--- core/src/utils/tsdk-runtime.js
+++ core/src/utils/tsdk-runtime.js
@@ -8,8 +8,8 @@ const { performance } = require('node:perf_hooks');
 
 const { ensureDataDir } = require('../config/runtime-paths');
 
-const OFFICIAL_VERSION = 'v3.9.0.1788935757';
-const OFFICIAL_SHA256 = '529ddc57ec69bd0b9f515ca79780e21523ea7eb7743362e55df67232255f0716';
+const OFFICIAL_VERSION = 'v3.9.0.1789137379';
+const OFFICIAL_SHA256 = '1744e339d43425f9f24834fd49b3239f824f57fe76242d5b3128ac55b3110ac5';
 const DEFAULT_APP_ID = '1112386029';
 const DEFAULT_GAME_ID = 3167;
 const DEFAULT_APP_KEY = '0';
@@ -83,7 +83,7 @@ class TsdkRuntime {
         this.appId = String(options.appId || DEFAULT_APP_ID);
         this.gameId = Number(options.gameId || process.env.FARM_TSDK_GAME_ID || DEFAULT_GAME_ID);
         this.appKey = String(options.appKey ?? process.env.FARM_TSDK_APP_KEY ?? DEFAULT_APP_KEY);
-        this.wasmPath = options.wasmPath || path.join(__dirname, 'tsdk-v3.9.0.1788935757.wasm');
+        this.wasmPath = options.wasmPath || path.join(__dirname, 'tsdk-v3.9.0.1789137379.wasm');
         this.dataDir = options.dataDir || path.join(ensureDataDir(), 'tsdk', this.accountId);
         this.deviceInfo = options.deviceInfo || {};
         this.logger = typeof options.logger === 'function' ? options.logger : () => {};
